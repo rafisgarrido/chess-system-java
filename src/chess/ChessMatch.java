053596222e4c775d446c8b724e23ef100a348135
rafisgarrido <rafisgarrido@gmail.com>
2023-11-13 16:35:13 -0300

    "Moving pieces"

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -23,6 +25,29 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+
+    public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition){
+        Position source = sourcePosition.toPosition();
+        Position target = targetPosition.toPosition();
+        //Operacao responsavel por validar se essa posicao de origem existe, caso contrario uma excecao sera lancada
+        validateSourcePosition(source);
+
+        //Operacao responsavel por realizar o movimento da peca, para essa operacao temos posicao de origem e de destino
+        Piece capturedPiece = makeMove(source, target);
+        return (ChessPiece) capturedPiece;
+    }
+
+    private Piece makeMove(Position source, Position target){
+        Piece p =board.removePiece(source);
+        Piece capturedPiece = board.removePiece(target);
+        board.placePiece(p, target);
+        return capturedPiece;
+    }
+    private void validateSourcePosition(Position position){
+        if (!board.positionExists(position)){
+            throw new ChessException("There is no piece on source position.");
+        }
     }
 
     //Metodo para receber as coordenadas do xadrez
