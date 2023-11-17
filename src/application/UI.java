745d3f78e1c260eac5933f96d750c4de5929f40e
rafisgarrido <rafisgarrido@gmail.com>
2023-11-17 09:04:31 -0300

    "Queen"

--- src/application/UI.java
+++ src/application/UI.java
@@ -102,7 +102,7 @@ public class UI {
             System.out.print(ANSI_BLUE_BACKGROUND);
         }
         if (piece == null) {
-            System.out.print("-");
+            System.out.print("-" + ANSI_RESET);
         }
         else {
             if (piece.getColor() == Color.WHITE) {
